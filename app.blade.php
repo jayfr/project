c2669fbe3c3192641cda65c5a7d3d93dfb0e1158
jayfr <49937426+jayfr@users.noreply.github.com>
2020-02-28 19:48:26 +0100

    Add files via upload

---
+++ app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" >
+<title>@yield('title')</title>
+</head>
+<body>
+<ul>
+<li><a href="/about">about me</a></li>
+<li><a href="/services">SUP</a></li>
+
+</ul>
+ @yield('content')
+</body>
+</html>
